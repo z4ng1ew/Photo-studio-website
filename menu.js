a91dbe95745081d10e739d3bd0d75cb2e1548a33
z4ng1ew <z4ng1ew@gmail.com>
2024-04-13 13:55:17 +0300

    first commit

--- menu.js
+++ menu.js
@@ -1,17 +1,16 @@
-$(document).ready(function() {
-        $('.menu-burger__header').click(function() {
-            $('.menu-burger__header').toggleClass('open-menu');
-        });
-});
-$(document).ready(function() {
-        $('.menu-burger__header').click(function(){
-            $('.menu-burger__header').toggleClass('open-menu');
-            $('.header__nav').toggleClass('open-menu');
-        });
-});
-$(document).ready(function() {
-        $('.header__burger').click(function(event){
-            $('.header__burger, .header__menu').toggleClass('active');
-            $('body').toggleClass('fixed-page');
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.menu-burger__header').addEventListener('click', function() {
+        document.querySelector('.menu-burger__header').classList.toggle('open-menu');
     });
+
+    document.querySelector('.menu-burger__header').addEventListener('click', function() {
+        document.querySelector('.menu-burger__header').classList.toggle('open-menu');
+        document.querySelector('.header__nav').classList.toggle('open-menu');
+    });
+
+    document.querySelector('.header__burger').addEventListener('click', function(event) {
+        document.querySelector('.header__burger').classList.toggle('active');
+        document.querySelector('.header__menu').classList.toggle('active');
+        document.body.classList.toggle('fixed-page');
+    });
+});
